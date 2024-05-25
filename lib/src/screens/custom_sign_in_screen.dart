95f5de368dfceb6014fc01625eb292d35694d9d3
karim <karim.tarek96@yahoo.com>
2024-05-25 16:59:11 +0300

    setup of firebase sdk and cli

--- lib/src/screens/custom_sign_in_screen.dart
+++ lib/src/screens/custom_sign_in_screen.dart
@@ -8,7 +8,7 @@ class CustomSignInScreen extends ConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Sign in'),
+        title:const Center(child:  Text('Sign in')),
       ),
     );
   }
